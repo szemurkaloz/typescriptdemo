b79b7b8fd9b3c2fbf28ab1ae40a25e372797042b
szemurkaloz@freemail.hu <szemurkaloz@freemail.hu>
2016-04-14 08:42:21 +0200

    Meló közben mentés

---
+++ typings/tsd.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="mocha/mocha.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="should/should.d.ts" />
+/// <reference path="superagent/superagent.d.ts" />
+/// <reference path="supertest/supertest.d.ts" />
